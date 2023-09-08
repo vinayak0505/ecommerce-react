2b7aa62c6867e9c1fbc9e1abb17f9504db12d4e0
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-09-08 19:09:14 +0530

    fixed color

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -19,18 +19,26 @@ const Nav = ({ isLoggedIn, setIsLoggedIn }) => {
         // An error happened.
       });
   };
-  
-  
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
-        <div className="container">
-          <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">
+      <nav className="navbar navbar-expand-lg navbar-light  py-3 sticky-top nav">
+        <div className="container ">
+          <NavLink className="navbar-brand text-light fw-bold fs-4 px-2" to="/">
             React Ecommerce
           </NavLink>
 
           <div className="" id="navbarSupportedContent">
             <div className="buttons text-center">
+              <NavLink to="/" className="btn btn-outline-dark m-2">
+                <i className="fa fa-user-plus mr-1"></i> Home
+              </NavLink>
+              <NavLink to="/bought" className="btn btn-outline-dark m-2">
+                <i className="fa fa-user-plus mr-1"></i> Bought
+              </NavLink>
+              <NavLink to="/cart" className="btn btn-outline-dark m-2">
+                <i className="fa fa-cart-shopping mr-1"></i> Cart
+              </NavLink>
               {isLoggedIn ? (
                 <div
                   onClick={handleLogout}
@@ -43,12 +51,6 @@ const Nav = ({ isLoggedIn, setIsLoggedIn }) => {
                   <i className="fa fa-sign-in-alt mr-1"></i> Login
                 </NavLink>
               )}
-              <NavLink to="/bought" className="btn btn-outline-dark m-2">
-                <i className="fa fa-user-plus mr-1"></i> Bought
-              </NavLink>
-              <NavLink to="/cart" className="btn btn-outline-dark m-2">
-                <i className="fa fa-cart-shopping mr-1"></i> Cart
-              </NavLink>
             </div>
           </div>
         </div>
