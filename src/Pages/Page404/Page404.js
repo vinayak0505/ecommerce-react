596ccc0a0a52cc4a85fc9f918935e92262e9b6a5
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-04-27 01:43:06 +0530

    added error page

--- src/Pages/Page404/Page404.js
+++ src/Pages/Page404/Page404.js
@@ -1,6 +1,9 @@
 const Error404 = () => {
     // error page to show that error has occured
-    return <>Error404</>;
+    return <>
+        <>Error404</>;
+        <p>Redirecting to <a href="/">homepage</a>...</p>
+    </>
 };
 
 export default Error404;
