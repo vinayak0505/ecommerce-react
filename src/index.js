e98db43febd89e5584ca61e455a9795d913f6805
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-04-27 01:53:30 +0530

    removed router

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
